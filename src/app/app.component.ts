ad06f8bffa25a06e860eb89d25c1231101de6e17
Snehal Bhavsar <Snehal.Bhavsar@samudratech.local>
2025-02-11 17:04:55 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule,RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule,HomeComponent,RouterOutlet,NavbarComponent,AboutComponent,ServicesComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path :'' , redirectTo:'home',pathMatch:'full'},
+{path:'home' , component:HomeComponent},
+{path:'about' , component:AboutComponent},
+{path:'Services' , component:ServicesComponent},
+{path:'Contact' , component:ContactComponent}
+
+ 
+];
+
